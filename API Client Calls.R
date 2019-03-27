f16f0fd0d4ce79a107bac4d3c084f351953425fc
Sam Fenton <sam_fenton@btinternet.com>
2019-03-27 16:10:37 +0000

    Code added for using Clustal Omega Client

--- API Client Calls.R
+++ API Client Calls.R
@@ -17,6 +17,12 @@ phobius(email= 'test@ebi.ac.uk',
         stype= 'protein', 
         sequence= 'sp:phyb_arath')
 
+source("clustalo.R")
+
+clustalo(email= 'test@ebi.ac.uk',
+         sequence= './sequence/prot_sequences.fasta',
+         stype= 'protein')
+
 # Order that parameters are put in doesn't matter if the parameter being given a value is specified 
 # to specify mulitple outformats for example you need to do outformat= c("accs", "ids")
 # doing with outformat= "accs", "ids" will cause ids to be taken as the input for the next parameter in the list
