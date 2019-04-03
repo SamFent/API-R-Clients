5f835e9f98aaffe6bc0bcfd217efc196f1e083e6
Sam Fenton <sam_fenton@btinternet.com>
2019-04-03 18:18:56 +0100

    Code added for using Simple Phylogeny R Client

--- API Client Calls.R
+++ API Client Calls.R
@@ -23,6 +23,12 @@ clustalo(email= 'test@ebi.ac.uk',
          sequence= 'sp:pak4_human,sp:pak2_human,sp:pak5_human',
          stype= 'protein')
 
+source("simple_phylogeny.R")
+
+simple_phylogeny(email= 'test@ebi.ac.uk',
+                 sequence= "./Sequence/prot_sequences.aln")
+
+
 # Order that parameters are put in doesn't matter if the parameter being given a value is specified 
 # to specify mulitple outformats for example you need to do outformat= c("accs", "ids")
 # doing with outformat= "accs", "ids" will cause ids to be taken as the input for the next parameter in the list
