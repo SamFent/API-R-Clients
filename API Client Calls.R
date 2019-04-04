20ed886dcf9d1725266a7e2a393e0842895fa1c7
Sam Fenton <sam_fenton@btinternet.com>
2019-04-04 17:42:45 +0100

    Added new tool and workflow code

--- API Client Calls.R
+++ API Client Calls.R
@@ -8,7 +8,8 @@ ncbiblast(email= 'test@ebi.ac.uk',
           sequence= 'sp:pak4_human',
           stype= 'protein',
           database= 'uniprotkb_swissprot',
-          program= 'blastp')
+          program= 'blastp',
+          outformat= c("accs", "ids"))
 
 
 source("phobius.R")
@@ -17,18 +18,28 @@ phobius(email= 'test@ebi.ac.uk',
         stype= 'protein', 
         sequence= 'sp:phyb_arath')
 
+source("emboss_backtranseq.R")
+
+emboss_backtranseq(email= "test@ebi.ac.uk",
+               sequence= "./Sequence/prot_sequence.fasta",
+               outformat= "sequence")
+
+
+# Order that parameters are put in doesn't matter as long as each wanted parameter is specified in the call
+# to specify mulitple outformats,  you need to do outformat= c("accs", "ids")
+
+
+# Simple Workflow Example 
+
 source("clustalo.R")
+source("simple_phylogeny.R")
 
 clustalo(email= 'test@ebi.ac.uk',
          sequence= 'sp:pak4_human,sp:pak2_human,sp:pak5_human',
-         stype= 'protein')
-
-source("simple_phylogeny.R")
-
+         stype= 'protein',
+         outformat= "aln-clustal_num",
+         outfile= "clustal") & 
 simple_phylogeny(email= 'test@ebi.ac.uk',
-                 sequence= "./Sequence/prot_sequences.aln")
+         sequence= "clustal.aln-clustal_num.clustal_num")
 
-
-# Order that parameters are put in doesn't matter if the parameter being given a value is specified 
-# to specify mulitple outformats for example you need to do outformat= c("accs", "ids")
-# doing with outformat= "accs", "ids" will cause ids to be taken as the input for the next parameter in the list
+# Note: This gives output from both steps 
